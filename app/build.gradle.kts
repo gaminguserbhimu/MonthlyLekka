1a04acde01b072936753534127461162e01966d3
Vinaykrishna <bhimappakunthe@gmail.com>
2026-09-12 08:59:56 +0530

    chore: Bump app version to v1.0.3 (versionCode 4) for Aptoide release update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.vinay.monthlylekka"
         minSdk = 26
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
